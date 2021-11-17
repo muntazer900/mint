731c9ec1ff2aa47bf48bc0b7c576c5d95dc10bb3
muntazer900 <93286974+muntazer900@users.noreply.github.com>
2021-11-17 08:03:17 +0300

    Update playlist.py

--- program/playlist.py
+++ program/playlist.py
@@ -3,10 +3,20 @@
 # Finished On 28/10/2021
 
 from config import BOT_USERNAME
-from pyrogram.types import Message
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
 from pyrogram import Client, filters
 from driver.queues import QUEUE, get_queue
 from driver.filters import command, other_filters
+
+
+keyboard = InlineKeyboardMarkup(
+    [[InlineKeyboardButton("🗑 اغلاق", callback_data="cls")]]
+)
 
 
 @Client.on_message(command(["الانتضار", f"playlist@{BOT_USERNAME}", "queue", f"queue@{BOT_USERNAME}"]) & other_filters)
@@ -15,7 +25,7 @@ async def playlist(client, m: Message):
    if chat_id in QUEUE:
       chat_queue = get_queue(chat_id)
       if len(chat_queue)==1:
-         await m.reply(f"💡 **يتم التشغيل حاليا:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`", disable_web_page_preview=True)
+         await m.reply(f"💡 **يتم التشغيل الان:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`", reply_markup=keyboard)
       else:
          QUE = f"💡 **في قائمة الانتضار:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**📖 قائمة الانتضار:**\n"
          l = len(chat_queue)
@@ -24,6 +34,6 @@ async def playlist(client, m: Message):
             hok = chat_queue[x][2]
             hap = chat_queue[x][3]
             QUE = QUE + "\n" + f"**#{x}** - [{han}]({hok}) | `{hap}`"
-         await m.reply(QUE, disable_web_page_preview=True)
+         await m.reply(QUE, reply_markup=keyboard)
    else:
       await m.reply("❌ **ماكو شي مشتغل دعبل.**")
